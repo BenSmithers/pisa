gradient fixes, splitting of flux

pisa/stages/flux/barr_gradients.py
@@ -63,7 +63,6 @@ class barr_gradients(Stage):
             for key in dfile.keys():
                 grads[param][key] = np.array(dfile[key][:])
 
-
         for param in self._barr_params:
             for container in self.data:
                 """
@@ -75,10 +74,17 @@ class barr_gradients(Stage):
                 if container.size==0:
                     continue
                 
-                interp = RectBivariateSpline( grads[param]["costh_nodes"], grads[param]["energy_nodes"],grads[param][container.name.split("_")[0]])
+                if container["nubar"]<0:
+                    e_effect = RectBivariateSpline( grads[param]["costh_nodes"], grads[param]["energy_nodes"],grads[param]["nuebar"])
+                    mu_effect = RectBivariateSpline( grads[param]["costh_nodes"], grads[param]["energy_nodes"],grads[param]["numubar"])
+                else:
+                    e_effect = RectBivariateSpline( grads[param]["costh_nodes"], grads[param]["energy_nodes"],grads[param]["nue"])
+                    mu_effect = RectBivariateSpline( grads[param]["costh_nodes"], grads[param]["energy_nodes"],grads[param]["numu"])
                 
                 #logging.debug("shape out is {} versus {} expected".format(np.shape(out), container.size))
-                container[param] =  interp(container["true_coszen"], container["true_energy"], grid=False)
+                # no tau effect since there are no conventional taus 
+                container[param] = (e_effect(container["true_coszen"], container["true_energy"], grid=False)*container["flux_contribution"][:,0] 
+                                        + mu_effect(container["true_coszen"], container["true_energy"], grid=False)*container["flux_contribution"][:,1])
 
                 container.mark_changed(param)
 
@@ -87,12 +93,12 @@ class barr_gradients(Stage):
         for container in self.data:
             # modify container[flux_key]
             container["barr_scale"] = ( self.params.conv_norm.value.m_as("dimensionless") *
-                (1+container["barr_grad_WP"]*self.params.barr_grad_WP.value.m_as("dimensionless")) * 
-                (1+container["barr_grad_WM"]*self.params.barr_grad_WM.value.m_as("dimensionless")) *   
-                (1+container["barr_grad_YP"]*self.params.barr_grad_YP.value.m_as("dimensionless")) * 
-                (1+container["barr_grad_YM"]*self.params.barr_grad_YM.value.m_as("dimensionless")) * 
-                (1+container["barr_grad_ZP"]*self.params.barr_grad_ZP.value.m_as("dimensionless")) * 
-                (1+container["barr_grad_ZM"]*self.params.barr_grad_ZM.value.m_as("dimensionless")) 
+                (1+container["weighted_aeff"]*container["barr_grad_WP"]*self.params.barr_grad_WP.value.m_as("dimensionless")) * 
+                (1+container["weighted_aeff"]*container["barr_grad_WM"]*self.params.barr_grad_WM.value.m_as("dimensionless")) *   
+                (1+container["weighted_aeff"]*container["barr_grad_YP"]*self.params.barr_grad_YP.value.m_as("dimensionless")) * 
+                (1+container["weighted_aeff"]*container["barr_grad_YM"]*self.params.barr_grad_YM.value.m_as("dimensionless")) * 
+                (1+container["weighted_aeff"]*container["barr_grad_ZP"]*self.params.barr_grad_ZP.value.m_as("dimensionless")) * 
+                (1+container["weighted_aeff"]*container["barr_grad_ZM"]*self.params.barr_grad_ZM.value.m_as("dimensionless")) 
                 )
             #container["barr_scale"][container["barr_scale"] < 0.0]=0.0
             

pisa/stages/flux/h5squid.py
@@ -50,6 +50,7 @@ class h5squid(Stage):
             dmsq:float,
             fluxfile:str,
             convmode=True,
+            separate_flux=True,
             rel_err=None,
             abs_err=None,
             **std_kwargs):
@@ -63,6 +64,7 @@ class h5squid(Stage):
         self.theta24 = FTYPE(th24)
         self.theta34 = FTYPE(th34)
         self.dmsq = FTYPE(dmsq) 
+        self.separate_flux = separate_flux
 
         assert self.num_neutrinos<5, "Only up to four neutrinos are supported"
         
@@ -117,17 +119,22 @@ class h5squid(Stage):
 
         return this_osc
 
-    def get_initial_state(self)->np.ndarray:
+    def get_initial_state(self, only_flavor=-1)->np.ndarray:
         """
             Returns the initial state for nusquids. Assumes the energies are provided in eV
             and the zeniths as cos(zeniths)
+
+            if `only_flavor` is provided, this will skip all flavors that aren't that flavor
         """
+
         flavors = self.num_neutrinos
         neutrinos = 2
         inistate = np.zeros(shape=(flavors, neutrinos,len(self.zeniths),  len(self.energies) ))
         logging.debug("shape of inistate: {}".format(np.shape(inistate)))
         for i_flav in range(flavors):
             for j_nu in range(neutrinos):
+                if (only_flavor!=-1) and (only_flavor!=i_flav):
+                    continue
                 key = ""
                 if self._convmode:
                     key = "conv_"
@@ -188,9 +195,25 @@ class h5squid(Stage):
         self.energies = np.logspace(log10(min_e), log10(max_e), e_nodes)
         self.zeniths = np.linspace(-1, 1, cth_nodes)
 
-        self.squid_atm = self.setup_squid()
-        self.squid_atm.Set_initial_state(self.get_initial_state(), nsq.Basis.flavor)
-        self.squid_atm.EvolveState()
+        logging.debug("flux status: {}".format(self.separate_flux))
+        if self.separate_flux:
+            logging.debug("Separate flux!")
+            self.e_atm = self.setup_squid()
+            self.e_atm.Set_initial_state(self.get_initial_state(0), nsq.Basis.flavor)
+            self.e_atm.EvolveState()
+
+            self.mu_atm = self.setup_squid()
+            self.mu_atm.Set_initial_state(self.get_initial_state(1), nsq.Basis.flavor)
+            self.mu_atm.EvolveState()
+
+            if not self._convmode: # no conventional taus...
+                self.tau_atm = self.setup_squid()
+                self.tau_atm.Set_initial_state(self.get_initial_state(2), nsq.Basis.flavor)
+                self.tau_atm.EvolveState()
+        else:
+            self.squid_atm = self.setup_squid()
+            self.squid_atm.Set_initial_state(self.get_initial_state(), nsq.Basis.flavor)
+            self.squid_atm.EvolveState()
 
         self.data.representation = self.calc_mode
         if self.data.is_map:
@@ -204,6 +227,7 @@ class h5squid(Stage):
 
 
         for container in self.data:
+            container["flux_contribution"] = np.zeros((container.size, 3), dtype=FTYPE)
             container['evt_flux'] = np.zeros(container.size, dtype=FTYPE)
 
         # don't forget to un-link everything again
@@ -219,12 +243,31 @@ class h5squid(Stage):
             for i in range(container.shape[0]):
                 
                 i_nu = 0 if container["nubar"] < 0 else 1
-                container["evt_flux"][i] = self.squid_atm.EvalFlavor( container["flav"], container["true_coszen"][i],  scale_e[i], i_nu )
-          
+
+                if self.separate_flux:
+                    container["flux_contribution"][i][0] = self.e_atm.EvalFlavor( container["flav"], container["true_coszen"][i],  scale_e[i], i_nu )
+                    container["flux_contribution"][i][1] = self.mu_atm.EvalFlavor( container["flav"], container["true_coszen"][i],  scale_e[i], i_nu )
+                    if not self._convmode:
+                        container["flux_contribution"][i][2] = self.tau_atm.EvalFlavor( container["flav"], container["true_coszen"][i],  scale_e[i], i_nu )
+
+                    norm = np.sum(container["flux_contribution"][i])
+                    container["evt_flux"] = norm
+
+                    container["flux_contribution"][i][0] /= norm
+                    container["flux_contribution"][i][1] /= norm
+                    if not self._convmode:
+                        container["flux_contribution"][i][2] /= norm
+
+
+                else:
+                    container["flux_contribution"][i][container["flav"]] = 1.0
+                    container["evt_flux"][i] = self.squid_atm.EvalFlavor( container["flav"], container["true_coszen"][i],  scale_e[i], i_nu )
             
+            container.mark_changed("flux_contribution")
             container.mark_changed("evt_flux")
+
             if (container.size!=0):
-                logging.debug("Min and max {} and {}".format(np.min(container["evt_flux"]), np.max(container["evt_flux"])))
+                logging.debug("Min and max {} and {}".format(np.min(container["flux_contribution"]), np.max(container["flux_contribution"])))
 
     def apply_function(self):
         for container in self.data: